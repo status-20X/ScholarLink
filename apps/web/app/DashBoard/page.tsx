a0ff1fdf6e54747b4a9666719408af3ad4a70946
soorajk1 <ksooraj162@gmail.com>
2023-10-01 13:12:11 +0530

    fixed dashboard bug where background didnt increase

--- apps/web/app/DashBoard/page.tsx
+++ apps/web/app/DashBoard/page.tsx
@@ -1,27 +1,34 @@
-"use client"
-import I from '../dp.jpg'
-import React from 'react'
+"use client";
+import I from "../dp.jpg";
+import React from "react";
 import "tailwindcss/tailwind.css";
 import "../style/globals.css";
-import {useEffect , useState} from 'react'
-import Image from 'next/image';
-const Dashboard= () => {
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import DashboardComponent from "../DashboardComponent/DashboardComponent";
+const Dashboard = () => {
   return (
-    <div className='bg-[#1E1E1E] min-w-full h-screen flex items-center justify-center'>
-      <div  className=' bg-[#D9D9D9] rounded-[2.5rem] w-[80%] flex flex-col items-center justify
-      -center'>
-        <div className='rounded-full flex justify-center h-[30rem] w-[30rem] bg-black p-4'>
-          <Image src= {I} alt="ada" className='rounded-full self-center w-[95%] h-[95%]'/>
+    <div className="bg-[#1E1E1E] min-w-full h-fit flex items-center justify-center">
+      <div
+        className=" bg-[#D9D9D9] rounded-[2.5rem] w-[80%] flex flex-col items-center justify
+      -center"
+      >
+        <div className="rounded-full flex justify-center h-[30rem] w-[30rem] bg-black p-4">
+          <Image
+            src={I}
+            alt="ada"
+            className="rounded-full self-center w-[95%] h-[95%]"
+          />
         </div>
-        <div className='rounded-xl bg-black text-white mt-10 text-center text-9xl'>Arjit Kedia</div>
-        <div className='bg-black rounded-[15rem] min-h-[6.25rem] min-w-[22.5rem] m-6 text-white flex flex-col justify-center items-center text-xl'>
-          <div>Scheme</div>
-          <div>Status</div>
-          <div>extra</div>
+        <div className="rounded-xl bg-black text-white mt-10 text-center text-9xl">
+          Arjit Kedia
         </div>
+        <DashboardComponent scheme="scheme" status="pending" />
+        <DashboardComponent scheme="scheme" status="resolved" />
+        <DashboardComponent scheme="scheme" status="pending" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
