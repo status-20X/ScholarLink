b270d42d9dd35f682dd3e7aefac5aa7bb5cb8558
Tushar <tusharkaler450@gmail.com>
2023-10-05 00:24:29 +0530

    fixing for build

--- apps/web/app/Homepage/page.tsx
+++ apps/web/app/Homepage/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import React from 'react'
 import "tailwindcss/tailwind.css";
 import "../style/globals.css";
 import Navbar from '../Navbar/page';
--- apps/web/app/StatusComponent/page.tsx
+++ apps/web/app/StatusComponent/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import "tailwindcss/tailwind.css";
 import "../style/globals.css";
